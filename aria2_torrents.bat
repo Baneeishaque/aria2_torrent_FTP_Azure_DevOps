e505712d05e2aa12a0619b42fce58587e1db3da1
Banee Ishaque K <Baneeishaque@gmail.com>
2018-12-11 07:49:53 +0530

    Transformers

--- aria2_torrents.bat
+++ aria2_torrents.bat
@@ -2,7 +2,7 @@
 
 for /f "tokens=1* delims=\" %%A in (
   'forfiles /s /m *.torrent /c "cmd /c echo @relpath"'
-) do for %%F in (^"%%B) do aria2c.exe --seed-time=0 --dir=aria2_torrent_ftp_vsts_repository %%~F
+) do for %%F in (^"%%B) do aria2c.exe --seed-time=0 --dir=aria2_torrent_ftp_vsts_repository "%%~F"
 
 DIR /s aria2_torrent_ftp_vsts_repository 
 :: Pause 
